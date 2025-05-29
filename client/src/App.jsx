7023d27c5249dd792c15b2a494966b7df34b255e
Minh-Khoi-Le <minh-khoi.le.1@ens.etsmtl.ca>
2025-05-28 22:20:08 -0400

    added Frontend and docker-compose

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import Button from 'react-bootstrap/Button';
+import Alert from 'react-bootstrap/Alert';
+
+function App() {
+  return (
+    <div className="container mt-4">
+      <Alert variant="success">
+        Frontend React + Bootstrap fonctionne !
+      </Alert>
+      <Button variant="primary">Essayer Bootstrap</Button>
+    </div>
+  );
+}
+
+export default App;
